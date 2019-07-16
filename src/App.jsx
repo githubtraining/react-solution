6eceeeb8309cfcb8229be192787bb3621b066b06
Parth Shah <partyshah@Parths-MacBook-Pro.local>
2019-07-16 06:27:01 -0700

    final solution

---
+++ src/App.jsx
@@ -0,0 +1,127 @@
+import React from "react";
+import Table from "./Table";
+import List from "./List";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      buttonClicked: "",
+      assignments: [],
+      students: [],
+      grades: {}
+    };
+
+    this.handleButtonClicked = this.handleButtonClicked.bind(this);
+    this.addAssignment = this.addAssignment.bind(this);
+    this.addStudent = this.addStudent.bind(this);
+    this.addGrade = this.addGrade.bind(this);
+  }
+
+  handleButtonClicked(buttonName) {
+    this.setState({
+      buttonClicked: buttonName
+    });
+  }
+
+  /*Check out this addAssignment function for step 3*/
+  addAssignment(assignmentName) {
+    this.setState({
+      assignments: this.state.assignments.concat(assignmentName)
+    });
+  }
+
+  addStudent(studentName) {
+    this.setState({
+      students: this.state.students.concat(studentName)
+    });
+  }
+
+  addGrade(assignment, student, score) {
+    let grades = this.state.grades;
+    let assignmentName = assignment;
+    let studentName = student;
+    if (!(assignment in grades)) {
+      grades[assignmentName] = {};
+    }
+    grades[assignmentName][studentName] = score;
+    this.setState({ grades: grades });
+  }
+
+  render() {
+    let tabChoice = <div />;
+
+    /*Uncomment below for step 2*/
+    if (this.state.buttonClicked === "assignments") {
+      tabChoice = (
+        <List
+          placeholder="Add Assignment..."
+          currList={this.state.assignments}
+          addFunction={this.addAssignment}
+          title="Assignments"
+        />
+      );
+    }
+
+    /* Change below for step 4*/
+
+    if (this.state.buttonClicked === "students") {
+      tabChoice = (
+        <List
+          placeholder="Add Student..." 
+          currList={this.state.students}
+          addFunction={this.addStudent}
+          title="Student Roster"
+        />
+      );
+    }
+
+    /* Uncomment lines below for step 3*/
+    if (this.state.buttonClicked === "grades") {
+      tabChoice = (
+        <Table
+          tableNames={this.state.assignments}
+          rows={this.state.students}
+          addFunction={this.addGrade}
+          data={this.state.grades}
+        />
+      );
+    }
+
+    return (
+      <div>
+        <div className="Box Box--spacious f4">
+          <div className="Box-header">
+            <h3 className="Box-title d-flex flex-justify-center">GradeBook</h3>
+          </div>
+        </div>
+        <nav className="UnderlineNav d-flex flex-justify-center">
+          <div className="UnderlineNav-body pt-6">
+            <button
+              className="btn btn-primary"
+              onClick={() => this.handleButtonClicked("assignments")}
+            >
+              Assignments
+            </button>
+            <button
+              className="btn btn-primary"
+              onClick={() => this.handleButtonClicked("students")}
+            >
+              Students
+            </button>
+            <button
+              className="btn btn-primary"
+              onClick={() => this.handleButtonClicked("grades")}
+            >
+              Grades
+            </button>
+          </div>
+        </nav>
+        {tabChoice}
+      </div>
+    );
+  }
+}
+
+export default App;
